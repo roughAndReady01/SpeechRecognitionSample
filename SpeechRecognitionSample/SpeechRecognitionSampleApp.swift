5a9c3bfef54eae8341f1224df6acfb2ed2b1b8fd
Haruki Hanagata <haruzo28@icloud.com>
2023-01-12 07:38:48 +0900

    Initial Commit

---
+++ SpeechRecognitionSample/SpeechRecognitionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognitionSampleApp.swift
+//  SpeechRecognitionSample
+//
+//  Created by 花形春輝 on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognitionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
